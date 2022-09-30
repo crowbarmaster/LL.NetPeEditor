78bc3dbb4e70e1a444c45cecb8e77d3db74246f2
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-09-30 00:50:08 -0400

    Fix IAT table, Generate references for exports.

--- LL.NetPeEditor/Program.cs
+++ LL.NetPeEditor/Program.cs
@@ -14,7 +14,7 @@ string[] functionSkipPrefixes = new string[]{
 		"?__",
 		"??_",
 		"??@",
-		"?$TSS",
+		//"?$TSS",
 		"??_C",
 		"??3",
 		"??2",
@@ -33,29 +33,26 @@ PEFile bedrockFile = PEFile.FromFile("bedrock_server.exe");
 IPEImage bedrockImage = PEImage.FromFile(bedrockFile);
 PESection lastSection = bedrockFile.Sections.Last();
 
-PESection exportSection = new PESection("ExpSect", SectionFlags.MemoryRead | SectionFlags.ContentInitializedData);
 ExportDirectoryBuffer exportBuffer = new ExportDirectoryBuffer();
-ExportDirectory newExports = new ExportDirectory("ExpDir");
+ExportDirectory newExports = new ExportDirectory("bedrock_server.exe");
+
+
 foreach(var export in bedrockImage.Exports.Entries) {
-	ExportedSymbol sym = new ExportedSymbol(exportSection.Contents.ToReference());
+	ExportedSymbol sym = new ExportedSymbol(exportBuffer.ToReference());
 	sym.Address = export.Address;
 	sym.Name = export.Name;
 	newExports.Entries.Add(sym);
 }
 
+
 foreach (PdbPublicSymbol sym in pdbReader.PublicSymbols) {
 	if (IsFunctionExportable(sym)) {
-		ExportedSymbol newSym = new ExportedSymbol(exportSection.Contents.ToReference());
-		newSym.Name = sym.Name;
-		newSym.Address.UpdateOffsets((ulong)sym.Offset, (uint)sym.RelativeVirtualAddress);
+		ExportedSymbol newSym = new ExportedSymbol(bedrockFile.GetReferenceToRva((uint)sym.RelativeVirtualAddress), sym.Name);
 		newExports.Entries.Add(newSym);
-		//Console.WriteLine(newSym.Name);
 	}
 }
 
 exportBuffer.AddDirectory(newExports);
-exportSection.Contents = exportBuffer;
-bedrockFile.Sections.Add(exportSection);
 
 ImportDirectoryBuffer importDirectory = new ImportDirectoryBuffer(false);
 ImportedModule preloaderModule = new ImportedModule("LLPreLoader.dll");
@@ -67,23 +64,26 @@ foreach (var import in bedrockImage.Imports) {
 }
 importDirectory.AddModule(preloaderModule);
 
-PESection importSection = new PESection("ImpSect", SectionFlags.MemoryRead | SectionFlags.MemoryWrite | SectionFlags.ContentInitializedData);
+PESection importSection = new PESection("ImpFunc", SectionFlags.MemoryRead | SectionFlags.MemoryWrite);
+PESection expSection = new PESection("ExpFunc", SectionFlags.MemoryRead);
+PESection iatSection = new PESection("IatSect", SectionFlags.MemoryRead | SectionFlags.MemoryWrite);
+SegmentBuilder segment = new SegmentBuilder();
 importSection.Contents = importDirectory;
+expSection.Contents = exportBuffer;
+iatSection.Contents = importDirectory.ImportAddressDirectory;
+bedrockFile.Sections.Add(iatSection);
+bedrockFile.Sections.Add(expSection);
 bedrockFile.Sections.Add(importSection);
+
 bedrockFile.AlignSections();
 
-DataDirectory exportHeader = new DataDirectory(exportSection.Rva, exportSection.Contents.GetVirtualSize());
-DataDirectory importHeader = new DataDirectory(importSection.Rva, importSection.Contents.GetVirtualSize());
+DataDirectory exportHeader = new DataDirectory(exportBuffer.Rva, exportBuffer.GetVirtualSize());
+DataDirectory importHeader = new DataDirectory(importDirectory.Rva, importDirectory.GetVirtualSize());
+DataDirectory iatHeader = new DataDirectory(importDirectory.ImportAddressDirectory.Rva, importDirectory.ImportAddressDirectory.GetVirtualSize());
 bedrockFile.OptionalHeader.DataDirectories[(int)DataDirectoryIndex.ExportDirectory] = exportHeader;
 bedrockFile.OptionalHeader.DataDirectories[(int)DataDirectoryIndex.ImportDirectory] = importHeader;
+bedrockFile.OptionalHeader.DataDirectories[(int)DataDirectoryIndex.IatDirectory] = iatHeader;
 bedrockFile.UpdateHeaders();
-bedrockFile.Write("BedrockLLTest.exe");
-bedrockImage = PEImage.FromFile("BedrockLLTest.exe");
-foreach(var import in bedrockImage.Imports) {
-	if(import.Name == "LLPreLoader.dll") {
-		import.Symbols[0].AddressTableEntry.UpdateOffsets(importSection.Offset, importSection.Rva);
-    }
-}
 
 bedrockFile.Write("BedrockLLTest.exe");
 Console.WriteLine("Done.");
